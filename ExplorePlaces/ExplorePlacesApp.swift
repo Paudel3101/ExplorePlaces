458cdf76637f7cd265b8eb17e9c24ed5abce5a66
Paudel3101 <bishalsharma24112002@gmail.com>
2025-11-04 17:38:01 -0500

    Initial Commit

---
+++ ExplorePlaces/ExplorePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorePlacesApp.swift
+//  ExplorePlaces
+//
+//  Created by Bishal Paudel on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ExplorePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
